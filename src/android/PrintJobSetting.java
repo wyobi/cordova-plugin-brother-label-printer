8ccb5ae1ec306b9e19652c58e9da79ecebbe5c22
Arcadius Ahouansou <arcadius@menelic.com>
2017-08-10 13:58:02 +0100

    [AA] issue #2: Ability to lookup printer at runtime

--- src/android/PrintJobSetting.java
+++ src/android/PrintJobSetting.java
@@ -1,7 +1,14 @@
 package com.momzor.cordova.plugin.brotherPrinter;
 
 
+import android.support.annotation.NonNull;
+
+import com.brother.ptouch.sdk.LabelInfo;
 import com.brother.ptouch.sdk.PrinterInfo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PrintJobSetting {
     String modelName;
@@ -16,10 +23,57 @@ public class PrintJobSetting {
     int labelNameIndex;
     PrinterInfo.Orientation orientation;
 
-    PrintJobSetting(String modelName, PrinterInfo.Model model, int labelNameIndex, PrinterInfo.Orientation orientation) {
-        this.modelName = modelName;
-        this.model = model;
-        this.labelNameIndex = labelNameIndex;
-        this.orientation= orientation;
+    boolean printerFound;
+
+    protected void init(final JSONArray args) throws JSONException {
+        JSONObject options = args.getJSONObject(1);
+
+        String tmpModelName = options.getString("modelName").toUpperCase();
+        String tmpModelEnumName = tmpModelName.replace("-", "_");
+        String tmpLabelName = options.getString("labelName").toUpperCase();
+        String tmpOrientation = options.getString("orientation").toUpperCase();
+        String tmpPort = options.getString("port").toUpperCase();
+
+        this.numberOfCopies = toNumberOfCopies(options);
+        this.orientation = PrinterInfo.Orientation.valueOf(tmpOrientation);
+        this.modelName = tmpModelName;
+        this.model = PrinterInfo.Model.valueOf(tmpModelEnumName);
+        this.labelNameIndex = toLabelIndexName(tmpLabelName);
+        this.port = toPort(tmpPort);
+
+        this.printerFound = false;
+    }
+
+    private int toNumberOfCopies(JSONObject options) throws JSONException {
+        int n = options.getInt("numberOfCopies");
+        if (n <= 0) {
+            throw new IllegalArgumentException("Invalid numberOfCopies: " + n);
+        }
+
+        return n;
+    }
+
+    @NonNull
+    private PrinterInfo.Port toPort(String port) {
+        PrinterInfo.Port p = PrinterInfo.Port.valueOf(port);
+        if (PrinterInfo.Port.NET.equals(p) || PrinterInfo.Port.BLUETOOTH.equals(port)) {
+            return p;
+        }
+
+        throw new IllegalArgumentException("Unsupported port " + this.port);
+    }
+
+    private int toLabelIndexName(String labelName) {
+        if (this.modelName.startsWith("QL")) {
+            LabelInfo.QL700 label = LabelInfo.QL700.valueOf(labelName);
+            return label.ordinal();
+        }
+
+        if (this.modelName.startsWith("PT")) {
+            LabelInfo.PT label = LabelInfo.PT.valueOf(labelName);
+            return label.ordinal();
+        }
+
+        throw new IllegalArgumentException("Unsupported printer model " + this.modelName);
     }
 }
--- www/printer.js
+++ www/printer.js
@@ -1,21 +1,22 @@
 var BrotherPrinter = function () {}
 BrotherPrinter.prototype = {
-
-    printViaBluetooth: function (data, numberOfCopies, callback) {
-        if (!data || !data.length) {
-            console.log('No data passed in. Expects a bitmap.')
-            return
-        }
-        cordova.exec(callback, function(err){console.log('error: '+err)}, 'BrotherPrinter', 'printViaBluetooth', [data, numberOfCopies])
-    },
-
-    printViaNetwork: function (data, numberOfCopies, callback) {
-        if (!data || !data.length) {
-            console.log('No data passed in. Expects a bitmap.')
-            return
-        }
-        cordova.exec(callback, function(err){console.log('error: '+err)}, 'BrotherPrinter', 'printViaNetwork', [data, numberOfCopies])
+  printLabel: function (data, options, callback) {
+    if (!data || !data.length) {
+      console.log('No data passed in. Expects a bitmap.')
+      return;
     }
+
+    if (!options || !options.numberOfCopies || !options.orientation || !options.labelName || !options.modelName || !options.port) {
+      console.log(`Invalid print options... Options should be for instance in the format: '{"numberOfCopies":1, "orientation":"LANDSCAPE", "labelName":"W62RB", "modelName":"QL-820NBW", "port":"NET"}'.`)
+      return;
+    }
+
+    cordova.exec(callback,
+      function (err) {
+        console.log('error: ' + err)
+      },
+      'BrotherPrinter', 'printLabel', [data, options])
+  },
 
 }
 var plugin = new BrotherPrinter()
