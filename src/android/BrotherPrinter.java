d4d572b2bdba8b661d026cfeb2962029ed97ebec
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2024-09-02 12:45:29 +0200

    Fix Support for QL_810W on Android

--- src/android/BrotherPrinter.java
+++ src/android/BrotherPrinter.java
@@ -60,6 +60,7 @@ public class BrotherPrinter extends CordovaPlugin {
 
     private static PrinterInfo.Model[] supportedModels = {
             PrinterInfo.Model.QL_720NW,
+            PrinterInfo.Model.QL_810W,
             PrinterInfo.Model.QL_820NWB,
             PrinterInfo.Model.QL_1110NWB,
             PrinterInfo.Model.TD_2120N
